5d4284c2c8f148c7da73b22cb72bf7ff52b53f5e
tnt007tarun <tnt.tarun@gmail.com>
2015-06-20 14:31:06 -0400

    whats going on

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,11 @@
+class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
+	def index
+	@tags = Tag.all
+	end
+
+	def show
+  	@tag = Tag.find(params[:id])
+	end
+
+end
